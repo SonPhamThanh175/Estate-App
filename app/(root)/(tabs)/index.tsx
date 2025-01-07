d0d57d6ad399e7a6bfbeb55fd1095407fcfb0505
SonDev1 <leha82349@gmail.com>
2025-01-07 10:09:02 +0700

    feat: new screen home

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,63 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Card, FeaturedCard } from '@/components/Cards';
+import Search from '@/components/Search';
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import { useGlobalContext } from '@/lib/global-provider';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className="font-bold text-lg my-10">Welcome To My App</Text>
-      <Link href="/sign-in"> Sign In</Link>
-      <Link href="/explore"> Explore</Link>
-      <Link href="/profile"> Profile</Link>
-      <Link href="/properties/1"> Property</Link>
-    </View>
-  );
+    const { user, refetch } = useGlobalContext();
+    return (
+        <SafeAreaView className='w-full bg-white'>
+            <View className='px-5 '>
+                <View className='flex flex-row items-center justify-between mt-5'>
+                    <View className='flex flex-row items-center'>
+                        \
+                        <Image
+                            source={{ uri: user.avatar }}
+                            className='size-12 rounded-full'
+                        />
+                        <View className='flex flex-col items-center ml-2 justify-center'>
+                            <Text className='text-xs font-rubik text-black-100'>Good Morning</Text>
+                            <Text className='text-base font-rubik-medium text-black-300'>
+                                {user?.name}
+                            </Text>
+                        </View>
+                    </View>
+                    <Image
+                        source={icons.bell}
+                        className='size-6'
+                    />
+                </View>
+                <Search />
+                <View className='my-5'>
+                    <View className='flex flex-row items-center justify-between'>
+                        <Text className='text-xl font-rubik-bold text-black-300'>Featured</Text>
+                        <TouchableOpacity>
+                            <Text className='text-base font-rubik-bold text-primary-300'>
+                                See All
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View className='flex flex-row gap-5 mt-5'>
+                    <FeaturedCard/>
+                    <FeaturedCard/>
+                    <FeaturedCard/>
+                    </View>
+                    </View>    
+                <View className='flex flex-row items-center justify-between'>
+                        <Text className='text-xl font-rubik-bold text-black-300'>Our Recommendation</Text>
+                        <TouchableOpacity>
+                            <Text className='text-base font-rubik-bold text-primary-300'>
+                                See All
+                            </Text>
+                        </TouchableOpacity>
+                </View>
+                <View className='flex flex-row gap-5 mt-5'>
+                    <Card/>
+                    <Card/>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
-
-
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,32 @@
-import { SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-import "./globals.css";
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import './globals.css';
+import GlobalProvider from '@/lib/global-provider';
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    'Rubik-Bold': require('../assets/fonts/Rubik-Bold.ttf'),
-    'Rubik-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
-    'Rubik-Light': require('../assets/fonts/Rubik-Light.ttf'),
-    'Rubik-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
-    'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
-    'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf')
-  });
+    const [fontsLoaded] = useFonts({
+        'Rubik-Bold': require('../assets/fonts/Rubik-Bold.ttf'),
+        'Rubik-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
+        'Rubik-Light': require('../assets/fonts/Rubik-Light.ttf'),
+        'Rubik-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
+        'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
+        'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
+    });
 
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync(); // Ẩn splash screen khi font đã được tải
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync(); // Ẩn splash screen khi font đã được tải
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null; // Hoặc có thể hiển thị một splash screen tùy chỉnh ở đây
     }
-  }, [fontsLoaded]);
 
-  if (!fontsLoaded) {
-    return null; // Hoặc có thể hiển thị một splash screen tùy chỉnh ở đây
-  }
-
-  return (
-    <Stack>
-      {/* Các màn hình của bạn sẽ ở đây */}
-    </Stack>
-  );
+    return (
+        <GlobalProvider>
+            <Stack screenOptions={{ headerShown: false }} />;
+        </GlobalProvider>
+    )
 }
